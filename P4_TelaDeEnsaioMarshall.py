c41635c7f776ee2181c0df3043b5b78db760e3b2
LucioMi <121905292+LucioMi@users.noreply.github.com>
2023-02-26 21:22:54 -0300

    c

--- P4_TelaDeEnsaioMarshall.py
+++ P4_TelaDeEnsaioMarshall.py
@@ -16,7 +16,7 @@ from subprocess import run                                            #bibliotec
                                           VARIAVEIS DO PROGRAMA
 ====================================================================================================================="""
 tela4 = Tk()
-conexao = pymysql.connect ( host='localhost', user='root', passwd='',database='db_prensa_software')
+conexao = pymysql.connect(host='localhost', user='root', passwd='', database='db_prensa_software')
 cursor = conexao.cursor()
 SearchingPorts = True
 GetForcaFlag = False
@@ -35,7 +35,7 @@ def send_byte(byte):
     if F_Auxiliares.is_open():
         F_Auxiliares.write_byte(byte)
     else:
-        messagem.botton("Por favor, abra a porta serial!","yellow")
+        messagem.botton("Por favor, abra a porta serial!", "yellow")
 
 def send_Funcionamento():
     if F_Auxiliares.is_open():
@@ -43,7 +43,7 @@ def send_Funcionamento():
         for idx in range(0, len(DadosTx)):
             F_Auxiliares.write_byte(DadosTx[idx])
         else:
-            messagem.botton("Por favor, abra a porta serial!","yellow")
+            messagem.botton("Por favor, abra a porta serial!", "yellow")
 
 def f_receiving_serial():
     global Grafico_Forca
@@ -57,7 +57,7 @@ def f_receiving_serial():
             try:
                 serial_data = F_Auxiliares.read_line()
                 serial_data = str(serial_data)
-                serial_data = serial_data.replace("b'","")
+                serial_data = serial_data.replace("b'", "")
                 serial_data = serial_data.replace("r", "")
                 serial_data = serial_data.replace("\\", "")
                 serial_data = serial_data.replace("n'", "")
@@ -68,7 +68,7 @@ def f_receiving_serial():
                 messagem.forca(str(serial_data))
                 serial_data2 = F_Auxiliares.read_line()
                 serial_data2 = str(serial_data2)
-                serial_data2 = serial_data2.replace("b'","")
+                serial_data2 = serial_data2.replace("b'", "")
                 serial_data2 = serial_data2.replace("r", "")
                 serial_data2 = serial_data2.replace("\\", "")
                 serial_data2 = serial_data2.replace("n'", "")
@@ -86,16 +86,16 @@ def f_searching_ports():
     while True:
         sleep(0.3)
         if SearchingPorts:
-            messagem.botton('AGUARDE, procurando COM ports...',"red")
+            messagem.botton('AGUARDE, procurando COM ports...', "red")
             ports = serial.tools.list_ports.comports(include_links=False)
-            port_list.delete('0','end')
+            port_list.delete('0', 'end')
             idx = 0
             for port in sorted(ports):
-                port_list.insert(idx,port.device)
+                port_list.insert(idx, port.device)
                 idx = idx + 1
                 port_list["height"] = idx
                 SearchingPorts = False
-                messagem.botton('BUSCA FINALIZADA, escolha COM/BAUD',"blue")
+                messagem.botton('BUSCA FINALIZADA, escolha COM/BAUD', "blue")
 
 def LigaMar():
     global ax
@@ -108,7 +108,7 @@ def LigaMar():
         canva.get_tk_widget().place(width=1068, height=482, x=250, y=130)
         anima = animation.FuncAnimation(figura, animar, interval=1000, frames=10)
     else:
-        messagebox.showwarning("ERRO!!!!!","Verifique se os dados foram preenchidos corretamente")
+        messagebox.showwarning("ERRO!!!!!", "Verifique se os dados foram preenchidos corretamente")
         messagem.botton("Por favor, abra a porta serial!", "red")
 
 def ParaMar():
@@ -132,9 +132,9 @@ def Relatorio():
     global x1
     global y2
     cursor.execute("TRUNCATE TABLE teste;")
-    for r in range(0,len(x1)):
+    for r in range(0, len(x1)):
         sql = f'INSERT INTO teste(forca_t,deslocamento_t) VALUES (%s,%s)'
-        sql_data = [x1[r],y2[r]]
+        sql_data = [x1[r], y2[r]]
         cursor.execute(sql, sql_data)
         conexao.commit()
     messagebox.showwarning("RELATORIO GERADO COM SUCESSO",                     # CRIA UMA CAIXA COM UMA MENSAGEM DE ERRO
@@ -155,13 +155,13 @@ def Conectar():
     if not F_Auxiliares.is_open():
         F_Auxiliares.open()
         if F_Auxiliares.is_open():
-            messagem.botton("CONECTADO!","green")
+            messagem.botton("CONECTADO!", "green")
             GetForcaFlag = True
             if not receiving_serial.is_alive():
                 receiving_serial.start()
     else:
         F_Auxiliares.close()
-        messagem.botton("DESCONECTADO!","red")
+        messagem.botton("DESCONECTADO!", "red")
 
 def animar(i):
     global x
@@ -179,12 +179,12 @@ def animar(i):
         cont = 1
     if len(x) == len(y):
         ax.clear()
-        ax.plot(y, x,ls='-',lw=2,marker='o')
+        ax.plot(y, x, ls='-', lw=2, marker='o')
         ax.axis([0, 25.5, 0, 5500])
         ax.grid(True)
-        ax.set_title('GRAFICO: FORÇA(Kg/f) x DESLOCAMENTO(mm)',fontsize=28)
-        ax.set_xlabel('DESLOCAMENTO (mm)',fontsize=22)
-        ax.set_ylabel('FORÇA (Kg/F)',fontsize=22)
+        ax.set_title('GRAFICO: FORÇA(Kg/f) x DESLOCAMENTO(mm)', fontsize=28)
+        ax.set_xlabel('DESLOCAMENTO (mm)', fontsize=22)
+        ax.set_ylabel('FORÇA (Kg/F)', fontsize=22)
         x1 = x
         y2 = y
     max = None
@@ -205,25 +205,25 @@ def pararAutomatico():
                      CRIAÇÃO DE WIDGETS, LAYOUT DA TELA, CONEXÃO COM O BD E COMUNICAÇÃO SERIAL
 ====================================================================================================================="""
 img_fundo = PhotoImage(file="Tela_Ensaio_Marshall.png")
-label_fundo = Label(tela4,image=img_fundo)
-label_fundo.place(x=0,y=0)
+label_fundo = Label(tela4, image=img_fundo)
+label_fundo.place(x=0, y=0)
 
-B_LigaMar = Button(tela4,text="INICIAR ENSAIO",bg="dark green",bd=4,font=("Arial",18),command=LigaMar)           #botoes
-B_LigaMar.place(x=19,y=507)
-B_ParaMar = Button(tela4,text="PARAR ENSAIO",bg="red",font=("Arial",18),bd=5,command=ParaMar)
-B_ParaMar.place(x=19,y=569)
-B_Voltar = Button(tela4,text="      VOLTAR      ",bd=4,bg="yellow",font=("Arial",18),command=Voltar)
-B_Voltar.place(x=19,y=631)
-B_Buscar = Button(tela4,text="Buscar",bd=4,bg="blue",font=("Arial",18),command=Buscar)
+B_LigaMar = Button(tela4, text="INICIAR ENSAIO", bg="dark green", bd=4, font=("Arial", 18), command=LigaMar)    #botoes
+B_LigaMar.place(x=19, y=507)
+B_ParaMar = Button(tela4, text="PARAR ENSAIO", bg="red", font=("Arial", 18), bd=5, command=ParaMar)
+B_ParaMar.place(x=19, y=569)
+B_Voltar = Button(tela4, text="      VOLTAR      ", bd=4, bg="yellow", font=("Arial", 18), command=Voltar)
+B_Voltar.place(x=19, y=631)
+B_Buscar = Button(tela4, text="Buscar", bd=4, bg="blue", font=("Arial", 18), command=Buscar)
 B_Buscar.place(width=108, height=38, x=71, y=397)
-B_Conectar = Button(tela4,text="Conectar",bd=4,bg="blue",font=("Arial",16),command=Conectar)
+B_Conectar = Button(tela4, text="Conectar", bd=4, bg="blue", font=("Arial", 16), command=Conectar)
 B_Conectar.place(width=108, height=39, x=71, y=441)
 
-port_list = Listbox(tela4,height=1,width=7,bd=10,font="Arial 10",bg="black",fg="#008000",    #Cria widget do tipo lista
-                    highlightcolor="black",highlightthickness=0,highlightbackground="black")
+port_list = Listbox(tela4, height=1, width=7, bd=10, font="Arial 10", bg="black", fg="#008000",  #Cria widget do tipo lista
+                    highlightcolor="black", highlightthickness=0, highlightbackground="black")
 port_list.place(width=104, height=132, x=71, y=253)
-port_list.insert(END,"----")                                      #Define oque vai aparecer quando a lista estiver vazia
-port_list.bind('<Double-Button>', lambda e: messagem.port("green",port_list.get(ANCHOR)))
+port_list.insert(END, "----")                                      #Define oque vai aparecer quando a lista estiver vazia
+port_list.bind('<Double-Button>', lambda e: messagem.port("green", port_list.get(ANCHOR)))
 
 searching_ports = threading.Thread(target=f_searching_ports)                             #Busca por dispositivos seriais
 searching_ports.daemon = True
